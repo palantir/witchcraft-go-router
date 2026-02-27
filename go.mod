a650d0c16f934ec8c7d7264545e9fa6bb1656416
Tony Abboud <tabboud@palantir.com>
2026-02-27 13:18:28 -0800

    wgs to wgr

--- go.mod
+++ go.mod
@@ -1,3 +1,22 @@
 module github.com/palantir/witchcraft-go-router
 
 go 1.25.0
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/palantir/pkg/metrics v1.10.1
+	github.com/palantir/witchcraft-go-logging v1.65.0
+	github.com/stretchr/testify v1.11.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/palantir/go-metrics v1.1.1 // indirect
+	github.com/palantir/pkg v1.1.0 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ vendor/github.com/palantir/pkg/metrics/context.go
@@ -0,0 +1,84 @@
+// Copyright (c) 2018 Palantir Technologies. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package metrics
+
+import (
+	"context"
+)
+
+type mContextKey string
+
+const (
+	registryKey = mContextKey("metrics-registry")
+)
+
+var DefaultMetricsRegistry = NewRootMetricsRegistry()
+
+func WithRegistry(ctx context.Context, registry Registry) context.Context {
+	if container, ok := ctx.Value(registryKey).(*registryContainer); ok {
+		return context.WithValue(ctx, registryKey, &registryContainer{
+			Registry: registry,
+			Tags:     container.Tags,
+		})
+	}
+	return context.WithValue(ctx, registryKey, &registryContainer{
+		Registry: registry,
+	})
+}
+
+func FromContext(ctx context.Context) Registry {
+	prev, ok := ctx.Value(registryKey).(*registryContainer)
+	if !ok {
+		return DefaultMetricsRegistry
+	}
+	registry, ok := prev.Registry.(*rootRegistry)
+	if !ok {
+		return prev.Registry
+	}
+	if len(prev.Tags) == 0 {
+		return registry
+	}
+	return &childRegistry{
+		root: registry,
+		tags: prev.Tags,
+	}
+}
+
+// AddTags adds the provided tags to the provided context. If no tags are provided, the context is returned unchanged.
+// Otherwise, a new context is returned with the new tags appended to any tags stored on the parent context.
+// This function does not perform any de-duplication (that is, if a tag in the provided tags has the
+// same key as an existing one, it will still be appended).
+func AddTags(ctx context.Context, tags ...Tag) context.Context {
+	if len(tags) == 0 {
+		return ctx
+	}
+	container, ok := ctx.Value(registryKey).(*registryContainer)
+	if !ok || container == nil {
+		return context.WithValue(ctx, registryKey, &registryContainer{
+			Registry: DefaultMetricsRegistry,
+			Tags:     tags,
+		})
+	}
+	newTags := make(Tags, len(container.Tags)+len(tags))
+	copy(newTags, container.Tags)
+	copy(newTags[len(container.Tags):], tags)
+	return context.WithValue(ctx, registryKey, &registryContainer{
+		Registry: container.Registry,
+		Tags:     newTags,
+	})
+}
+
+// TagsFromContext returns the tags stored on the provided context. May be nil if no tags have been set on the context.
+func TagsFromContext(ctx context.Context) Tags {
+	if container, ok := ctx.Value(registryKey).(*registryContainer); ok {
+		return container.Tags
+	}
+	return nil
+}
+
+type registryContainer struct {
+	Registry Registry
+	Tags     Tags
+}
